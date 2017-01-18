25aa0b34d255d9cee96a4c720ce9768c295ed8d9
Kaz Matthews and Sam Kirsch <sam@>
2017-01-18 11:15:05 -0800

    built vowelCheck function and consonantMove functions.

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,44 @@ var consonants = ["q", "w", "r", "t", "p", "s", "d", "f", "g", "h", "j", "k", "l
 var vowels = ["e", "u", "i", "o", "a", "E", "A", "I", "O", "U"]
 var y = ["y", "Y"]
 
-
 var translate = function(word) {
-  return (word + "ay");
+  console.log(vowelCheck(word));
+  if (vowelCheck(word) === true) {
+    return (word + "ay");
+  }
+  else {
+    return (consonantMove(word) + "ay");
+  }
 }
 
+var vowelCheck = function(word) {
+  // check for first character being a vowel
+  var vowel = false;
+  for(var index = 0; index < vowels.length; index ++) {
+    if (vowels[index] === word.charAt(0)) {
+      vowel = true;
+      return vowel;
+    }
+  }
+  return vowel;
+};
+
+var consonantMove = function(word) {
+  // takes !! single !! first consonants and moves them to the end of the word
+  var wordArray = word.split("");
+  wordArray.push(wordArray[0]);
+  wordArray.splice(0,1);
+  var newWord = wordArray.join("");
+  return newWord;
+}
 
 
 $(document).ready(function() {
   $("form#input-form").submit(function(event) {
     event.preventDefault();
-
     var userInput = $("#text-input").val();
-    $("#translation").text(translate(userInput));
+    var output = translate(userInput);
+    $("#translation").text(output);
 
   });
 });
