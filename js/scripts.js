37d91ac8e130b4438a48573d0ffa3a25e871daa1
Kaz Matthews and Sam Kirsch <kaz@>
2017-01-18 10:20:33 -0800

    insert arrays for the characters and make the site return the word plus -ay

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
+var consonants = ["q", "w", "r", "t", "p", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "Q", "W", "R", "T", "P", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"]
+var vowels = ["e", "u", "i", "o", "a", "E", "A", "I", "O", "U"]
+var y = ["y", "Y"]
+
+
 var translate = function(word) {
-  return word;
+  return (word + "ay");
 }
 
 
